b67a8ab99b9bdbb6ef5fac9a66ce5524f5e18c36
Adam Ghowiba <adamware99@hotmail.com>
2023-09-13 15:28:54 +0400

    feat: export session

--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/auth.module';
 export * from './lib/guards/authenticated.guard';
 export * from './lib/middleware/auth.middleware';
+export { createExpressSession } from './lib/session/create-session.helper';
